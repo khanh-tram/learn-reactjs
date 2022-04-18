9b6b0b82da038ce920b1c88058016f2663652265
TramHTK <khanhtram1812@gmail.com>
2022-04-18 19:54:33 +0700

    [tramhtk] Update code section 5.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const name = 'Tramlun';
+  const age = 18;
+  const isMale = false;
+
+  // Ko thể render 1 object trực tiếp lên màn hình được
+  // Chỉ render đc từng thuộc tính trong object đó lên thui
+  const student = {
+    name: 'Easy Frontend'
+  };
+  const colorList = ['red', 'green', 'blue']
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,14 +24,57 @@ function App() {
         </p>
         <h1>Dinh Yêu Châm</h1>
         <h1>Dinh Yêu Châm</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <p>Xin chao {name} - {age} - {isMale ? 'Male' : 'Female'}</p>
+
+        {isMale ? <p>Male</p> : <p>Female</p>}
+
+        {isMale && <p>Male</p>}
+        {!isMale && <p>Female</p>}
+
+        {/* Yêu cầu bỏ trong 1 thằng cha, có thể là thẻ div */}
+        {/* 3 dòng liền nhau */}
+        {!isMale && (
+          <div>
+            <p>Female 1</p>
+            <p>Female 2</p>
+            <p>Female 3</p>
+          </div>
+        )}
+
+        {/* Nếu ko thích bỏ dô thẻ div chỉ muốn hiển thị 3 dòng này thôi
+        thì nhớ import thư viện React và thuộc tính React.Fragment */}
+        {/* Muốn ko để trong thẻ div cha thì có Fragment */}
+        {/* 3 dòng cách nhau từng dòng một */}
+        {!isMale && (
+          <React.Fragment>
+            <p>Female 1</p>
+            <p>Female 2</p>
+            <p>Female 3</p>
+          </React.Fragment>
+        )}
+
+        {/* Viết tắt của React.Fragment */}
+        {/* 3 dòng cách nhau từng dòng một */}
+        {!isMale && (
+          <>
+            <p>Female 1</p>
+            <p>Female 2</p>
+            <p>Female 3</p>
+          </>
+        )}
+
+        <p>{student.name}</p>
+
+        {/* ul là danh sách ko có thứ tự */}
+        {/* li là mục của danh sách(component trong list) */}
+        <ul>
+          {colorList.map(color => (
+            // Inline style
+            // key là khóa độc nhất của từng component trong mảng
+            <li key={color} style={{ color }}>{color}</li>
+          ))}
+        </ul>
       </header>
     </div>
   );
